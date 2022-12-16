c68f9b47ae7b4537af7b0d520409f7ed8e35a893
Amadeus Lehmann <Amadeus Lehmann@DESKTOP-J8Q46LU>
2022-12-16 10:12:33 +0100

    added datastorage class mock

---
+++ WariusWebWernwedienung/Client/DataStorage.cs
@@ -0,0 +1,17 @@
+﻿namespace WariusWebWernwedienung.Client;
+
+public interface IDataStorage
+{
+    Task DeleteSection(SectionInformation sectionInformation);
+    Task UpdateSection(SectionInformation sectionInformation);
+}
+
+public class DataStorage : IDataStorage
+{
+    public async Task UpdateSection(SectionInformation sectionInformation)
+    {
+    }
+    public async Task DeleteSection(SectionInformation sectionInformation)
+    {
+    }
+}
