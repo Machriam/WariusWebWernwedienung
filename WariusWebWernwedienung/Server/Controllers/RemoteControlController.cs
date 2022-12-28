14c65f30271435ba4a7ca231f42543b453601a23
Amadeus Lehmann <it@peptides.de>
2022-12-28 22:23:47 +0100

    feature/14_ParameterReplacement: cherry picked the feature

--- WariusWebWernwedienung/Server/Controllers/RemoteControlController.cs
+++ WariusWebWernwedienung/Server/Controllers/RemoteControlController.cs
@@ -9,30 +9,41 @@ namespace WariusWebWernwedienung.Server.Controllers;
 public class RemoteControlController : ControllerBase
 {
     private const string ScriptFolderName = "ScriptFolder";
-    private readonly IConfiguration _configuration;
+    private const string ParameterReplacementsName = "ParameterReplacements";
+    private readonly string _scriptFolder;
+    private readonly Dictionary<string, string> _parameterReplacements = new();
 
     public RemoteControlController(IConfiguration configuration)
     {
-        _configuration = configuration;
+        _scriptFolder = configuration.GetConnectionString(ScriptFolderName)
+            ?? throw new Exception($"Connection {ScriptFolderName} must be defined in appsettings.json");
+        foreach (var (key, value) in configuration
+            .GetSection(ParameterReplacementsName).GetChildren()
+            .Select(c => (c.Key, c.Value))) _parameterReplacements.Add(key, value ?? "");
     }
 
     [HttpGet]
     public IEnumerable<string> GetExecutableScripts()
     {
-        var path = _configuration.GetConnectionString(ScriptFolderName)
-            ?? throw new Exception($"Connection {ScriptFolderName} must be defined in appsettings.json");
-        return Directory.GetFiles(path).Select(f => Path.GetFileName(f)).Where(p => p.EndsWith(".bat") || p.EndsWith(".exe"));
+        return Directory.GetFiles(_scriptFolder)
+            .Select(f => Path.GetFileName(f))
+            .Where(p => p.EndsWith(".bat") || p.EndsWith(".exe"));
     }
 
     [HttpPost]
     public bool Post([FromBody] RemoteControlParameter parameter)
     {
-        var filePath = _configuration.GetConnectionString(ScriptFolderName);
+        foreach (var key in _parameterReplacements.Keys)
+            parameter.Parameter = parameter.Parameter.Replace(key, _parameterReplacements[key]);
         var process = new Process
         {
-            StartInfo = new ProcessStartInfo(filePath + "\\" + Path.GetFileName(parameter.FileName),
+            StartInfo = new ProcessStartInfo(_scriptFolder + "\\" + Path.GetFileName(parameter.FileName),
                                              "\"" + parameter.Parameter + "\"")
         };
+#if DEBUG
+        Console.WriteLine(process.StartInfo.FileName);
+        Console.WriteLine(process.StartInfo.Arguments);
+#endif
         return process.Start();
     }
 }
