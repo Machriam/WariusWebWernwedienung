218e8fa2808ae1699b0c0f62d85f5013a5e5f38a
Your Name <you@example.com>
2025-03-30 22:46:32 +0200

    included the PinChangeInterruptHandler directly and fixed the ISR issue, by prepending changeInterrupt() with ICACHE_RAM_ATTR

--- WeulenWedienung/WeulenReceiver/src/RFReceiver.h
+++ WeulenWedienung/WeulenReceiver/src/RFReceiver.h
@@ -10,66 +10,71 @@
 
 #include <PinChangeInterruptHandler.h>
 
-enum {
-  MAX_PAYLOAD_SIZE = 80,
-  MIN_PACKAGE_SIZE = 4,
-  MAX_PACKAGE_SIZE = MAX_PAYLOAD_SIZE + MIN_PACKAGE_SIZE,
-  MAX_SENDER_ID = 31
+enum
+{
+	MAX_PAYLOAD_SIZE = 80,
+	MIN_PACKAGE_SIZE = 4,
+	MAX_PACKAGE_SIZE = MAX_PAYLOAD_SIZE + MIN_PACKAGE_SIZE,
+	MAX_SENDER_ID = 31
 };
 
-class RFReceiver : PinChangeInterruptHandler {
-    const byte inputPin;
-    const unsigned int pulseLimit;
+class RFReceiver : PinChangeInterruptHandler
+{
+	const byte inputPin;
+	const unsigned int pulseLimit;
 
-    // Input buffer and input state
-    byte shiftByte;
-    byte errorCorBuf[3];
-    byte bitCount, byteCount, errorCorBufCount;
-    unsigned long lastTimestamp;
-    bool packageStarted;
+	// Input buffer and input state
+	byte shiftByte;
+	byte errorCorBuf[3];
+	byte bitCount, byteCount, errorCorBufCount;
+	unsigned long lastTimestamp;
+	bool packageStarted;
 
-    byte inputBuf[MAX_PACKAGE_SIZE];
-    byte inputBufLen;
-    uint16_t checksum;
-    volatile bool inputBufReady;
-    byte changeCount;
+	byte inputBuf[MAX_PACKAGE_SIZE];
+	byte inputBufLen;
+	uint16_t checksum;
+	volatile bool inputBufReady;
+	byte changeCount;
 
-    // Used to filter out duplicate packages
-    byte prevPackageIds[MAX_SENDER_ID + 1];
+	// Used to filter out duplicate packages
+	byte prevPackageIds[MAX_SENDER_ID + 1];
 
-    byte recvDataRaw(byte * data);
+	byte recvDataRaw(byte *data);
 
-  public:
-    RFReceiver(byte inputPin, unsigned int pulseLength = 100) : inputPin(inputPin),
-        pulseLimit((pulseLength << 2) - (pulseLength >> 1)), shiftByte(0),
-        bitCount(0), byteCount(0), errorCorBufCount(0), lastTimestamp(0),
-        packageStarted(false), inputBufLen(0), checksum(0),
-        inputBufReady(false), changeCount(0) {
+public:
+	RFReceiver(byte inputPin, unsigned int pulseLength = 100) : inputPin(inputPin),
+																pulseLimit((pulseLength << 2) - (pulseLength >> 1)), shiftByte(0),
+																bitCount(0), byteCount(0), errorCorBufCount(0), lastTimestamp(0),
+																packageStarted(false), inputBufLen(0), checksum(0),
+																inputBufReady(false), changeCount(0)
+	{
+	}
+	void begin()
+	{
+		pinMode(inputPin, INPUT);
+		attachPCInterrupt(digitalPinToPCINT(inputPin));
+	}
 
-    }
-    void begin() {
-      pinMode(inputPin, INPUT);
-      attachPCInterrupt(digitalPinToPCINT(inputPin));
-    }
+	void stop()
+	{
+		detachPCInterrupt(digitalPinToPCINT(inputPin));
+	}
 
-    void stop() {
-      detachPCInterrupt(digitalPinToPCINT(inputPin));
-    }
+	/**
+	 * Returns true if a valid and deduplicated package is in the buffer, so
+	 * that a subsequent call to recvPackage() will not block.
+	 *
+	 * @returns True if recvPackage() will not block
+	 */
+	bool ready() const
+	{
+		return inputBufReady;
+	}
 
-    /**
-     * Returns true if a valid and deduplicated package is in the buffer, so
-     * that a subsequent call to recvPackage() will not block.
-     *
-     * @returns True if recvPackage() will not block
-     */
-    bool ready() const {
-      return inputBufReady;
-    }
+	byte recvPackage(byte *data, byte *pSenderId = 0, byte *pPackageId = 0);
 
-    byte recvPackage(byte * data, byte *pSenderId = 0, byte *pPackageId = 0);
-
-    void decodeByte(byte inputByte);
-    virtual void handlePCInterrupt(int8_t pcIntNum, bool value);
+	void decodeByte(byte inputByte);
+	virtual void handlePCInterrupt(int8_t pcIntNum, bool value);
 };
 
-#endif  /* RECEIVER_H */
+#endif /* RECEIVER_H */
--- WeulenWedienung/WeulenReceiver/src/main.cpp
+++ WeulenWedienung/WeulenReceiver/src/main.cpp
@@ -6,28 +6,30 @@ RFReceiver receiver(D3);
 
 void setup()
 {
-  Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.print("Setup done.");
-  receiver.begin();
+	Serial.begin(115200);
+	pinMode(LED_BUILTIN, OUTPUT);
+	Serial.println("");
+	Serial.println("Setup done.");
+	receiver.begin();
 }
 
 void loop()
 {
-  statusBlinking();
-  byte data[MAX_PACKAGE_SIZE];
-  byte from = 0;
-  byte packageId = 0;
-  if (!receiver.ready())
-    return;
+	statusBlinking();
+	Serial.println("Receive");
+	byte data[MAX_PACKAGE_SIZE];
+	byte from = 0;
+	byte packageId = 0;
+	if (!receiver.ready())
+		return;
 
-  Serial.println("Waiting");
-  byte len = receiver.recvPackage(data, &from, &packageId);
+	Serial.println("Waiting");
+	byte len = receiver.recvPackage(data, &from, &packageId);
 
-  Serial.println("");
-  Serial.print("Package: ");
-  Serial.println(packageId);
-  Serial.println("");
+	Serial.println("");
+	Serial.print("Package: ");
+	Serial.println(packageId);
+	Serial.println("");
 }
 
 static unsigned long s_previousMillis = 0;
@@ -35,13 +37,14 @@ static bool s_ledOn = false;
 
 void statusBlinking()
 {
-  if (millis() - s_previousMillis > 500)
-  {
-    if (s_ledOn)
-      digitalWrite(LED_BUILTIN, HIGH);
-    else
-      digitalWrite(LED_BUILTIN, LOW);
-    s_ledOn = !s_ledOn;
-    s_previousMillis = millis();
-  }
+	Serial.println("Blink");
+	if (millis() - s_previousMillis > 500)
+	{
+		if (s_ledOn)
+			digitalWrite(LED_BUILTIN, HIGH);
+		else
+			digitalWrite(LED_BUILTIN, LOW);
+		s_ledOn = !s_ledOn;
+		s_previousMillis = millis();
+	}
 }
